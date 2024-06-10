447ace54afa3d64119a99c4bd6db1c1a9636b305
bilal1718 <muhbil171845@gmail.com>
2024-06-10 14:19:49 +0500

    Config files

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'neutralSilver':'#F5F7FA',
+        'neutralDGrey':'#4D4D4D',
+        'brandPrimary':'#4CAF4F',
+        'neutralGrey':'#717171',
+        'gray900':'#18191F',
+      }
+    },
+  },
+  plugins: [
+    flowbite.plugin(),
+  ],
+}
