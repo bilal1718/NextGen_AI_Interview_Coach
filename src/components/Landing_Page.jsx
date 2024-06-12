2e66e61e5a9b7530a29bf5c33f90ff8d41cc3e37
bilal1718 <muhbil171845@gmail.com>
2024-06-12 16:19:38 +0500

    Moving all components in one comp

---
+++ src/components/Landing_Page.jsx
@@ -0,0 +1,22 @@
+import Home from './Home'
+import Navbar from './Navbar'
+import MyFooter from './Footer'
+import HowItWorks from './HowItWorks'
+import Features from './Features'
+import CallToAction from './CallToAction'
+
+function Landing_Page() {
+
+  return (
+    <>
+     <Navbar />
+     <Home />
+     <Features />
+     <CallToAction />
+     <HowItWorks />
+     <MyFooter />
+    </>
+  )
+}
+
+export default Landing_Page
