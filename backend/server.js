24e2574246af00d30fac0166f68aa95179423a9a
bilal1718 <muhbil171845@gmail.com>
2024-06-13 17:47:15 +0500

    Adding more fields

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,11 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://console.firebase.google.com/project/nextgen-ai-interview/firestore/databases/-default-/data/~2F'
 });
-
-
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173', // Your frontend's URL
+    credentials: true
+  }));
 app.use(bodyParser.json());
 const db = admin.firestore();
 
@@ -43,15 +44,17 @@ const db = admin.firestore();
 // });
 app.post('/register', async (req, res) => {
     try {
-        const { email, password, displayName } = req.body;
+        const { email, password, userName,firstName,lastName } = req.body;
         const userRecord = await admin.auth().createUser({
             email,
             password,
-            displayName
+            displayName: userName,
         });
         await db.collection('users').doc(userRecord.uid).set({
             email,
-            displayName
+            displayName: userName,
+            firstName,
+            lastName
         });
         res.status(201).send('User created successfully');
     } catch (error) {
@@ -65,13 +68,14 @@ app.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
         const userRecord = await admin.auth().getUserByEmail(email);
-        res.status(200).json(userRecord);
+        // Sign in the user with the provided email and password
+        const token = await admin.auth().createCustomToken(userRecord.uid);
+        res.status(200).json({ token });
     } catch (error) {
-        console.error('Error fetching user:', error.message);
-        res.status(404).send('User not found');
+        console.error('Error logging in:', error.message);
+        res.status(401).send('Incorrect email or password');
     }
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
